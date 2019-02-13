d130741cc2dfb3292da84dcb8c1ac2881ef9aeb2
Sean Lucius <seanlucius22@icloud.com>
2019-02-13 04:39:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+      pigeon_data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      }
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, details|
+    details.each do |detail, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute] ||= []
+        pigeon_list[name][attribute] << detail.to_s
+      end 
+    end 
+  end 
+  pigeon_list
 end
